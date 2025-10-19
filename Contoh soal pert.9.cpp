630e8a1a34d386275f3521dc836c6e45a24e692e
ZaharaNauli23 <zaharaibrahim117@gmail.com>
2025-10-20 06:40:14 +0700

    Add files via upload

---
+++ Contoh soal pert.9.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+struct SIMPUL {
+    int INFO;
+    struct SIMPUL *LINK;
+};
+
+SIMPUL *P, *FIRST, *LAST;
+
+void BUAT_SIMPUL(int);
+
+int main() {
+    int x;
+    cout << "Masukkan Data : ";
+    cin >> x;
+
+    BUAT_SIMPUL(x);
+    cout << "Data : " << P->INFO << endl;
+
+    return 0;
+}
+
+void BUAT_SIMPUL(int x) {
+    P = (SIMPUL *)malloc(sizeof(SIMPUL));
+    if (P != NULL)
+        P->INFO = x;
+    else
+        cout << "Pembuatan Simpul Gagal" << endl;
+}
+
